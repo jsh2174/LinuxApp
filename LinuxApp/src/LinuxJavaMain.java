4de78c8e3274886b7310d888b606621472c0a05d
bit <bit@DESKTOP-08RGQCM>
2018-03-28 15:21:34 +0900

    2

--- LinuxApp/src/LinuxJavaMain.java
+++ LinuxApp/src/LinuxJavaMain.java
@@ -4,7 +4,7 @@ public class LinuxJavaMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello  Linux !!"); 
-		
+		System.out.print("---- 마지막으로 수정  ---------------------- !!"); 
 
 	}
 
